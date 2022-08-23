a648b8579261c279263c81d899a0b97293239b07
robin-cybrosys <robin.cybrosys@gmail.com>
2022-08-23 17:38:34 +0530

    Aug 23:initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# website_hospital appointment
---
+++ __manifest__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Website Employee Payslip",
+    'application': "True",
+    'author': "The R",
+    'website': "http://www.cybrosys.com",
+    'sequence': "-1",
+    'licence': "LGPL-3",
+    'category': 'Uncategorized',
+    'version': '15.0.1.0.0',
+
+    # any module necessary for this one to work correctly
+    'depends': ['base', 'website', 'hr', 'hr_payroll_community'],
+
+    # always loaded
+    'data': [
+        'views/employee_portal_template.xml',
+        # 'views/appointments_page.xml',
+        # 'views/appointments_webpage_template.xml',
+    ],
+}
